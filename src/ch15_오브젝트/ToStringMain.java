e1a06e597cbdd9a9036d668b4b51f9e3eb7b1f3a
mini3100 <mini3100@naver.com>
2023-06-30 11:34:39 +0900

    Revert "230629 수업"
    This reverts commit f4ea88b2da0221f4d0f3917e9d5896d1d08ad679.

--- src/ch15_오브젝트/ToStringMain.java
+++
@@ -1,8 +0,0 @@
-package ch15_오브젝트;
-
-public class ToStringMain {
-    public static void main(String[] args) {
-        KoreanStudent koreanStudent = new KoreanStudent("20230001","김영훈");
-        System.out.println(koreanStudent);
-    }
-}
