dc43d836edf1d9bd63fbc43550364dd5d290ddaa
mini3100 <mini3100@naver.com>
2023-06-30 11:48:54 +0900

    230629 수업

---
+++ src/ch13_스태틱/MessageUtilMain.java
@@ -0,0 +1,29 @@
+package ch13_스태틱;
+
+
+public class MessageUtilMain {
+	
+	private int a;
+	
+	public static void main(String[] args) {
+		MessageUtilMain messageUtilMain = new MessageUtilMain();
+		
+		System.out.println(messageUtilMain.a);
+		
+//		Array01.main(null);
+		
+		MessageUtil messageUtil = new MessageUtil();
+//		
+//		messageUtil.sendMail();
+//		messageUtil.sendFile();
+		
+		MessageUtil.sendFile();
+		System.out.println();
+		
+		MessageUtil.data = "안녕";
+		System.out.println(MessageUtil.data);
+		
+		System.out.println(messageUtil.data);
+	}
+	
+}
